965a31bacad287d617678f146369366a45f0c004
stopbrother <stopbro0323@naver.com>
2024-12-04 18:04:00 +0900

    feat: 로그아웃 로직 추가

--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -7,7 +7,7 @@ export async function signInWithDiscord() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'discord',
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/callback`,
+      redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/auth/callback`,
     },
   });
 
@@ -16,4 +16,15 @@ export async function signInWithDiscord() {
   }
 
   if (error) console.log('error', error);
+}
+
+export async function signOut() {
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    // redirect error page
+    console.log('error', error);
+  }
 }
